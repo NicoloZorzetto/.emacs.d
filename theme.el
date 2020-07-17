b0f5b48d3bf4fc5780d86d09c65b606307af5118
Nicolò Zorzetto <galileo@localhost.localdomain>
2020-07-17 19:01:14 +0200

    theme, cleaning needed

---
+++ theme.el
@@ -0,0 +1,23 @@
+(tool-bar-mode 0)
+
+(scroll-bar-mode 0)
+
+(require 'uniquify)
+(setq uniquify-buffer-name-style 'forward)
+
+(defun mode-line-render (left right)
+  (let* ((available-width (- (window-width) (length left) )))
+    (format (format "%%s %%%ds" available-width) left right)))
+(setq-default mode-line-format
+     '((:eval
+       (mode-line-render
+       (format-mode-line (list
+         " %b "
+         (if (and buffer-file-name (buffer-modified-p))
+             (propertize "(modified)" 'face `(:inherit face-faded)))))
+       (format-mode-line
+        (propertize "%4l:%2c  " 'face `(:inherit face-faded)))))))
+
+(show-paren-mode t)
+
+(global-display-line-numbers-mode 1)

e86cb07684bf39a0bd7678820bef3326bc759e48
Nicolò Zorzetto <galileo@localhost.localdomain>
2020-07-20 00:34:04 +0200

    Added Ivy and reverted to (package-install 'package) because it loads faster and allows for better literature

--- init.el
+++ init.el
@@ -1,19 +1,13 @@
 
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-(package-initialize)
-
-(org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
-
+(org-babel-load-file "~/.emacs.d/myinit.org")
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (use-package)))
- '(send-mail-function (quote mailclient-send-it)))
+ '(package-selected-packages
+   (quote
+    (magit all-the-icons dashboard elpy modus-vivendi-theme use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- myinit.el
+++ myinit.el
@@ -1,3 +1,23 @@
-(global-set-key (kbd "M-5") "~")
+(package-initialize)
 
-(org-babel-load-file "~/.emacs.d/zemacs.org")
+(package-install 'use-package)
+
+(package-install 'all-the-icons)
+
+(package-install 'elpy)
+(require 'elpy)
+
+(package-install 'dashboard)
+
+(dashboard-setup-startup-hook)
+(setq dashboard-banner-logo-title "Welcome back Nicolò")
+;(setq dashboard-startup-banner [VALUE])
+(setq dashboard-center-content t)
+(setq dashboard-set-heading-icons t)
+(setq dashboard-set-file-icons t)
+(setq dashboard-set-footer nil)
+
+(package-install 'modus-vivendi-theme)
+(load-theme 'modus-vivendi t)
+
+(global-set-key (kbd "M-5") "~")
